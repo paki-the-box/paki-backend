21092f8aee30265f9d4df8e5bd865d033115d245
Tim Mitsch <t.mitsch@pragmaticindustries.de>
2020-10-31 01:23:05 +0100

    fixed a minor merge bug

--- pakibackend/paki/urls.py
+++ pakibackend/paki/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('locker/locations', LockerLocationsView.as_view(), name='locker-locations'),
     path('locker/find/lat/<str:latitude>/long/<str:longitude>/', LockerFindView.as_view() , name='locker-find'),
     path('shipment', ShipmentView.as_view(), name='shipment-create'),
-    path('shipment/<str:id>/', ShipmentView.as_view(), name='shipment-get')
+    path('shipment/<str:id>/', ShipmentView.as_view(), name='shipment-get'),
     path('user/add', user.UserAddView.as_view(), name='user-add'),
     path('user/get/<str:email>', user.UserGetView.as_view(), name='user-get')
 ]
