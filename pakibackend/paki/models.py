77871dad771985dbc6eb03f7f919f7c6a5107458
Tim Mitsch <t.mitsch@pragmaticindustries.de>
2020-10-31 11:21:44 +0100

    created contacts

--- pakibackend/paki/models.py
+++ pakibackend/paki/models.py
@@ -13,7 +13,30 @@ class HandoverTransaction(models.Model):
     transaction_state = models.CharField(max_length=255, null=False, choices=[('C', 'Created'), ('A', 'Accepted'), ('D', 'Denied'), ('DR', 'Dropped-Off'), ('PU','Picked-UP')])
     dropoff_date = models.DateField(null=False)
     pickup_date = models.DateField(null=True)
-    
+
+
+class Contacts(models.Model):
+    contactId = models.UUIDField(null=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    email = models.CharField(max_length=255, null=False, blank=False) # Mail of sender
+    pictureLink = models.CharField(max_length=1023)
+
+    class Meta:
+        db_table = 'paki_backend_contacts'
+        constraints = [
+            models.UniqueConstraint(fields=['email'], name='unique email contacts')
+        ]
+
+
+class FavBoxes(models.Model):
+    boxId = models.UUIDField(null=False)
+    contact = models.ForeignKey(Contacts, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'paki_backend_favoriteboxes'
+        constraints = [
+            models.UniqueConstraint(fields=['boxId', 'contact'], name='unique email contact combination')
+        ]
+
 
 class User(models.Model):
     firstName = models.CharField(max_length=255)
